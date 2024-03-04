00fc4f5d2439e46900041fa609a9e6e3adddc7ef
Henry Reyes <reyesh2@uci.edu>
2024-03-03 21:48:47 -0800

    fmt for seession/debugging

--- src-tauri/src/session.rs
+++ src-tauri/src/session.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
+use std::fmt;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Session {
@@ -13,7 +14,7 @@ pub struct Session {
 impl Session {
     pub fn new(description: String) -> Self {
         Session {
-            id: None,
+            id : None,
             description,
             start_time: Utc::now(),
             end_time: None,
@@ -25,5 +26,19 @@ impl Session {
         
         let duration = self.end_time.expect("Error").signed_duration_since(self.start_time);
         self.hours_worked = Some(duration.num_seconds() as f64 / 3600.0);
+    }
+}
+
+impl fmt::Display for Session {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Session {{ id: {}, description: {}, start_time: {}, end_time: {}, hours_worked: {} }}",
+            self.id.map_or("None".to_string(), |id| id.to_string()),
+            self.description,
+            self.start_time,
+            self.end_time.as_ref().map_or("In Progress".to_string(),|et| et.to_rfc3339()),
+            self.hours_worked.map_or(0.0, |hw| hw)
+        )
     }
 }
