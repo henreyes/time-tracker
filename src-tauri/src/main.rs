55b103f9f3a379d1eb45b775a2bee1d40a76ee71
Henry Reyes <reyesh2@uci.edu>
2024-03-03 21:57:54 -0800

    fixed bug that didnt update end time properly

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,39 +2,44 @@
 
 mod session;
 use crate::session::*;
-use std::{fs, io::{self, Read, Write}, path::Path};
 use rusqlite::{Connection, params};
 use std::result::Result as StdResult;
-use chrono::{NaiveDateTime, DateTime, Utc, TimeZone};
+use chrono::{DateTime, Utc};
 
 #[tauri::command]
-fn get_sessions() -> Result<String,String> {
+fn get_sessions() -> Result<String, String> {
     let conn = Connection::open("sessions.db").map_err(|e| e.to_string())?;
     let mut stmt = conn
-        .prepare("SELECT id, description, start_time, end_time, hours_worked FROM sessions  ORDER BY id DESC")
+        .prepare("SELECT id, description, start_time, end_time, hours_worked FROM sessions ORDER BY id DESC")
         .map_err(|e| e.to_string())?;
 
     let sessions_iter = stmt
         .query_map([], |row| {
+            let end_time: Option<String> = row.get(3)?;
+            let parsed_end_time = match end_time {
+                Some(et) => Some(DateTime::parse_from_rfc3339(&et).unwrap().with_timezone(&Utc)),
+                None => None,
+            };
             Ok(Session {
                 id: row.get(0)?,
                 description: row.get(1)?,
-                start_time:DateTime::parse_from_rfc3339(&row.get::<_, String>(2)?).map_err(|_| rusqlite::Error::InvalidColumnType(2, "start_time".to_owned(), rusqlite::types::Type::Text))?.with_timezone(&Utc),
-                end_time: Some(Utc::now()),
+                start_time: DateTime::parse_from_rfc3339(&row.get::<_, String>(2)?).unwrap().with_timezone(&Utc),
+                end_time: parsed_end_time,
                 hours_worked: row.get(4)?,
             })
         })
         .map_err(|e| e.to_string())?;
-    
+
     let sessions: Result<Vec<Session>, _> = sessions_iter.collect();
     match sessions {
         Ok(sessions) => {
-            serde_json::to_string(&sessions).map_err(|e| e.to_string())
-        },
-        Err(e) => Err(e.to_string()), 
+            for session in &sessions {
+                println!("{}", session);
+            }
+            serde_json::to_string(&sessions).map_err(|e| e.to_string())},
+        Err(e) => Err(e.to_string()),
     }
 }
-
 
 
 fn init_db() -> StdResult<(), String> {
@@ -72,15 +77,16 @@ fn start_session(description: String) -> Result<i32, String> {
     
     Ok(id) 
 }
+
 #[tauri::command]
-fn end_session(id: u32) -> Result<(), String> {
+fn end_session(id: i32) -> Result<(), String> {
     let conn = Connection::open("sessions.db").map_err(|e| e.to_string())?;
     conn.execute(
-       "UPDATE sessions GET end_time ?1 WHERE id = ?2",
+        "UPDATE sessions SET end_time = ?1 WHERE id = ?2",
         params![
             Utc::now().to_rfc3339(),
             id,
-        ]
+        ],
     ).map_err(|e| e.to_string())?;
     Ok(())
 }
